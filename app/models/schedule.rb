4477ab07721c4d6ab1bb45d889651ae9e5e369a4
daipotepan <hololive0311@gmail.com>
2025-11-10 13:48:20 +0900

    repull

---
+++ app/models/schedule.rb
@@ -0,0 +1,15 @@
+class Schedule < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :memo, length: { maximum: 500 }
+  validate :end_date_after_start_date
+
+  def end_date_after_start_date
+    return if end_date.blank? || start_date.blank?
+    if end_date < start_date
+      errors.add(:end_date, "は開始日より後に設定してください")
+      
+    end
+  end
+end
